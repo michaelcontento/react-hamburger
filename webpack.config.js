dc9c372cc3225841e99aa165ed8bc7cba4bbae20
Michael Contento <michaelcontento@gmail.com>
2015-03-24 08:06:03 +0100

    build stuff

---
+++ webpack.config.js
@@ -0,0 +1,65 @@
+'use strict';
+
+var webpack = require('webpack');
+
+var jsxLoaders = [
+    'babel-loader' +
+        '?cacheDirectory=true' +
+        '&loose=es6.forOf' +
+        '&experimental' +
+        '&optional[]=runtime' +
+        '&optional[]=validation.react' +
+        '&optional[]=spec.undefinedToVoid' +
+        '&optional[]=utility.deadCodeElimination'
+];
+
+var config = {
+    entry: __dirname + '/index.js',
+
+    output: {
+        path: __dirname + '/build/',
+        filename: 'index.js',
+        library: 'react-router',
+        libraryTarget: 'umd'
+    },
+
+    plugins: [
+        new webpack.DefinePlugin({
+            'process.env.NODE_ENV': JSON.stringify(process.env.NODE_ENV)
+        })
+    ],
+
+    resolve: {
+        extensions: [
+            '', '.webpack.js', '.web.js', '.js', // Webpack Default
+            '.jsx'
+        ],
+        unsafeCache: true
+    },
+
+    externals: [
+        {
+            "react": true,
+            "react-router": true
+        }
+    ],
+
+    module: {
+        loaders: [
+           { test: /\.jsx?$/, exclude: /node_modules/, loaders: jsxLoaders }
+        ],
+        noParse: /\.min\.(css|js)$/
+    }
+};
+
+if (process.env.NODE_ENV === 'production') {
+    config.plugins.push(
+        new webpack.optimize.UglifyJsPlugin({
+            compressor: {
+                warnings: false
+            }
+        })
+    );
+}
+
+module.exports = config;
